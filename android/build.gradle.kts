a7a3545b996b52b40c98e6a99720ec013fe08973
Adnan Khan <jackmarkiplier@gmail.com>
2025-04-18 17:52:48 -0400

    final build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -18,10 +28,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-buildscript {
-    dependencies {
-        classpath 'com.google.gms:google-services:4.3.15'
-    }
 }
